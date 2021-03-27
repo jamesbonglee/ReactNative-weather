a5f62cfcc26ce5b06385f7a9d42050d498e512ce
jamesbonglee <jamesbonglee@gmail.com>
2021-03-28 05:09:35 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,30 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-   return (
-      <View style={styles.container}>
-         <View style={styles.greenView}>{/* <Text>Hello</Text> */}</View>
-         <View style={styles.redView}>{/* <Text>Hello</Text> */}</View>
-         <StatusBar style="auto" />
-      </View>
-   );
+   return <Loading></Loading>;
 }
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: "grey",
-      // alignItems: "center",
-      justifyContent: "center",
-   },
-   greenView: {
-      flex: 1,
-      backgroundColor: "green",
-   },
-   redView: {
-      flex: 1,
-      backgroundColor: "red",
-   },
-});
