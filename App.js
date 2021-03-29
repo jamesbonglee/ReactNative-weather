fc94fe9511e7823ec27b2ef7e7a639a9bca00bd7
jamesbonglee <jamesbonglee@gmail.com>
2021-03-29 22:37:31 +0900

    #1.2 getting Location

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-   return <Loading></Loading>;
+// export default function App() {
+//    return <Loading></Loading>;
+// }
+
+export default class extends React.Component {
+   gerLocation = async () => {
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+   };
+   componentDidMount() {
+      this.gerLocation;
+   }
+   render() {
+      return <Loading></Loading>;
+   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ReactNative-weather
 
 Learnng React Native by building a weather app
+
+Use stack
+"React"
+"Expo"
+"React Native"
+"Axios"
+"Open Weather API"
