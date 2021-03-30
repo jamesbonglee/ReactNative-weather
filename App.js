2ebbbcb9b5e0287eef2ae12b3eb77cc433442376
jamesbonglee <jamesbonglee@gmail.com>
2021-03-31 05:32:51 +0900

    Hold this project because there is something wrong with the import

--- App.js
+++ App.js
@@ -3,30 +3,38 @@ import { Alert } from "react-native";
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+// import dotenv from "dotenv";
+import axios from "axios";
 
-// export default function App() {
-//    return <Loading></Loading>;
-// }
+const API_KEY = "86d7200d4ca48e04f3e196cea35d2372";
+// const API_KEY = process.env.WEATHER_KEY;
 
 export default class extends React.Component {
    state = {
       isLoading: true,
    };
-   gerLocation = async () => {
+
+   getWeather = async (latitude, longitude) => {
+      const { data } = await axios.get(
+         `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      );
+      console.log(data);
+   };
+   getLocation = async () => {
       try {
-         const response = await Location.requestPermissionsAsync();
-         console.log(response);
+         await Location.requestPermissionsAsync();
          const {
             coords: { latitude, longitude },
          } = await Location.getCurrentPositionAsync();
          //send to API and get weather , coords:{latitude,longitude} == coords.latitude, coords.longitude
+         this.getWeather(latitude, longitude);
          this.setState({ isLoading: false });
       } catch (error) {
          Alert.alert("can't find U", "So sada");
       }
    };
    componentDidMount() {
-      this.gerLocation();
+      this.getLocation();
    }
    render() {
       const { isLoading } = this.state;
